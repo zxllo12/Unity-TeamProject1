a22272a6a0ecd9ac712aeb9ac7418801926ec33f
zxllo12 <poapog123@naver.com>
2024-10-28 18:21:44 +0900

    [fix]스크립트 명 변경

---
+++ Assets/Worker/PTG/Scripts/Player_Controller.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 direction;
+    public float speed = 8;
+    public float jumpForce = 10;
+    public float gravity = -20;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    public Animator animator;
+    public Transform model;
+
+    void Update()
+    {
+        //Take the horizontal input to move the player
+        float hInput = Input.GetAxis("Horizontal");
+        direction.x = hInput * speed;
+        animator.SetFloat("speed", Mathf.Abs(hInput));
+
+        //Check if the player is on the ground
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Atk();
+        }
+
+        if (isGrounded)
+        {
+            direction.y = -1;
+            if (Input.GetButton("Jump"))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            direction.y += gravity * Time.deltaTime;//Add Gravity
+        }
+
+        //Flip the player
+        if (hInput != 0)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
+            model.rotation = newRotation;
+        }
+
+        //Move the player using the character controller
+        controller.Move(direction * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        //Jump
+        direction.y = jumpForce;
+    }
+
+    private void Atk()
+    {
+        animator.SetTrigger("Atk");
+    }
+}
