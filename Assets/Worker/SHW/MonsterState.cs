f65151d4550fa778cd5b1e6344b5237718254871
HawoonShin <sinhaoon@gmail.com>
2024-10-28 09:14:56 +0900

    [Feat] 몬스터 상태패턴 구현 완료
    몬스터들의 공통적인 상태들을 담당할 상태패턴의 구현을 완료했습니다.

--- Assets/Worker/SHW/MonsterState.cs
+++ Assets/Worker/SHW/MonsterState.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class MonsterState : MonoBehaviour
 {
     // 대기, 추적, 공격, 사망, 되돌아가기, 스킬, 이동, 피격
-    public enum State { Idle, Running, Attack, Return, Skill, Walking, Dead, ISHit }
+    public enum State { Idle, Running, Attack, Return, Skill, Walking, Dead, IsHit }
 
     // 현상태
     [SerializeField] State curState;
@@ -11,13 +12,11 @@ public class MonsterState : MonoBehaviour
     [SerializeField] GameObject player;
     // 기본 위치(임시)
     public Vector3 spawnPoint;
+    // 원거리 공격시 발사할 프리팹
+    [SerializeField] GameObject bulletPrefab;
 
     // 재생할 에니메이터
     [SerializeField] Animator animator;
-    // 사용할때 아래처럼 해시화 해서 사용
-    private static int idleHash = Animator.StringToHash("Idle");
-    private static int walkHash = Animator.StringToHash("Walk");
-
 
     // 데이터 테이블로 블러올 내용들
     // 사거리, 스킬여부, 공격력, 방어력, 체력, 공속, 이속, 스킬 쿨
@@ -32,26 +31,28 @@ public class MonsterState : MonoBehaviour
     [SerializeField] float rage;    // 추적거리
     [SerializeField] float attackRage;   // 공격 사거리
     [SerializeField] bool canSkill; // 스킬여부
-    [SerializeField] bool attackType;  // 공격 타입
+    [SerializeField] bool attackType;  // 공격 타입 true일 경우 원거리
+
+    // 임시 구역
+    [Header("Test")]
+    [SerializeField] float damage;  // 체력감소에 쓰일 데미지(임시)
 
     private void Awake()
     {
-        // 데이터 테이블에서 정보 가져오기
-
-        // 초기 지점 설정
-        spawnPoint = transform.position;
-
         // 애니메이터 갖고오기
         animator = GetComponent<Animator>();
+
+        // 스폰 포인트 저장
+        spawnPoint = transform.position;
+
+        // 플레이어 확인(일단 이름으로 플레이어 오브젝트 찾는다)
+        player = GameObject.Find("Player");
     }
 
     private void Update()
     {
-        // 추적할 플레이어 오브젝트?컴포넌트 찾기
-        // Transform playerPos = player.GetComponent<Transform>();
-
+        // 상태 확인용 디버그 로그
         Debug.Log(curState);
-   
 
         // 상태패턴
         switch (curState)
@@ -74,13 +75,24 @@ public class MonsterState : MonoBehaviour
             case State.Walking: // 이동 = 배회
                 Walking();
                 return;
-            case State.ISHit:   // 피격
+            case State.IsHit:   // 피격
                 IsHit();
                 return;
             case State.Dead:  // (임시) 사망
                 Dead();
                 return;
         }
+
+        // 기준점보다 왼쪽일 경우 
+        // 기준점보다 오른쪽일 경우
+
+        // 죽었을 경우
+        if (hp < 0)
+        {
+            curState = State.Dead;
+        }
+
+        // (TODO)스턴상태일 경우 불러올 함수 작성
     }
 
     public void Idle()
@@ -88,96 +100,166 @@ public class MonsterState : MonoBehaviour
         // 대기 애니메이션 모션 출력
         animator.SetBool("isIdle", true);
 
-        // 플레이어가 범위 내로 들어왔을 경우
-        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < rage)
+        StartCoroutine(WalkCoroutine());
+
+        // 일정 범위 내에 플레이어가 들어왔을 경우
+        if (Vector3.Distance(transform.position, player.transform.position) <= rage)
         {
-            curState = State.Running;
+            animator.SetBool("isIdle", false);  // 애니메이션 취소
+            curState = State.Running;   // 추적상태로 변환
         }
+    }
 
-        // 
-
-        // 일정 시간이 되면 
-        // 코루틴
+    // 대기모션과 배회모션의 코루틴
+    IEnumerator WalkCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        curState = State.Walking;
+        animator.SetBool("isIdle", false);
     }
 
     public void Running()
     {
-        // 뛰는 애니메이션 출력
-        // animator.SetFloat("speed", 1);
+        // 추적 애니메이션 실행
+        animator.SetBool("isRunning", true);
 
-        // 타겟 위치로 이동
+        StopAllCoroutines();
+
+        // 타겟(플레이어)를 향해서 이동
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
 
-        // 플레이어가 범위를 벗어났을 경우
-        if (Vector3.Distance(player.transform.position, gameObject.transform.position) > rage)
+        // 공격범위 내로 들어왔을 경우
+        if (Vector3.Distance(transform.position, player.transform.position) <= attackRage)
         {
-            curState = State.Return;
-        }
-
-        // 플레이어가 공격범위로 들어왔을 경우
-        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < attackRage)
-        {
+            animator.SetBool("isRunning", false);
             curState = State.Attack;
+        }
+        // 일정 범위 내에 플레이어가 넘어갈 경우
+        else if (Vector3.Distance(transform.position, player.transform.position) >= rage)
+        {
+            animator.SetBool("isRunning", false);  // 애니메이션 취소
+            curState = State.Idle;   // 추적상태로 변환
         }
     }
 
+    // 이동시 스폰지점으로 돌아가는 Return
     public void Return()
     {
-        // (TODO)걷기 애니메이션 출력
-
-        // 원래 자리로 이동
+        // 되돌아가는 상태 = 걷는 모션
+        animator.SetBool("isWalking", true);
+        // 스폰지점으로 다시 돌아감
         transform.position = Vector3.MoveTowards(transform.position, spawnPoint, speed * Time.deltaTime);
 
-        // 원래 자리로 이동 완료시 
-        if (transform.position == spawnPoint)
+        // 원래 지점으로 이동을 위한 회전
+        // 몬스터가 왼쪽을 바라보는 지점을 기준으로 설정됨
+        // 오른쪽을 바라볼 경우 문제발생..
+        if (transform.rotation.eulerAngles.y >= 260)
         {
+            transform.rotation = transform.rotation * Quaternion.Euler(0, 180f, 0);
+        }
+
+        // 일정 범위 내에 플레이어가 들어왔을 경우
+        if (Vector3.Distance(transform.position, player.transform.position) < rage)
+        {
+            animator.SetBool("isWalking", false);  // 애니메이션 취소
+            curState = State.Running;   // 추적상태로 변환
+        }
+
+        // 스폰포인트에 도착했을 경우
+        else if (transform.position == spawnPoint)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
             curState = State.Idle;
+            transform.rotation = transform.rotation * Quaternion.Euler(0, -180f, 0);
         }
     }
 
     public void Attack()
     {
-        // 공격 애니메이션 출력
+        // 공격 애니메이션
+        animator.SetBool("isAttacking", true);
 
-        // 근거리 원거리 확인
+        // 공격시 플레이어 공격에 대해 어떤식으로 할지 논의 필요
+        // (임시) 가져온 플레이어의 체력을 가져와서 깎는 방식
 
-        // 플레이어 체력 감소
+        // 원거리 일 경우
+        // 한번만 실행시켜야하는데 공격상태일때 계속 반복(수정필요)
+        if (attackType == true)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.forward, transform.rotation);
+            Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+            rigidbody.velocity = bullet.transform.forward * attackSpeed;
+        }
 
+        // 공격범위 벗어났을 경우
+        if (Vector3.Distance(transform.position, player.transform.position) >= attackRage)
+        {
+            animator.SetBool("isAttacking", false);
+            curState = State.Running;
+        }
     }
 
     public void Dead()
     {
-        // 체력이 다 닳을경우 사망
-        Destroy(gameObject);
+        // 사망 애니메이션
+        animator.SetBool("isDead", true);
+        animator.SetBool("isDead", false);
     }
 
     public void Skill()
     {
-        // 스킬쪽과 혐력해서 작성
-        // (이하는 임시) 
-        // 스킬북이 존재한다면
-        // 스킬 북 컴포넌트 중 이름이 같은 부분을 가져와
-        // 오브젝트의 이름과 비교하여 스킬을 가져오는 방식으로?
+        // 스킬이 있는 몬스터의 경우
     }
 
     public void Walking()
     {
-        // 이동 애니메이션 출력
+        // 걷기 애니메이션 
+        animator.SetBool("isWalking", true);
 
-        // 이동
+        // 앞으로 이동?
         transform.position += Vector3.left * speed * Time.deltaTime;
-        // 후 리턴상태로 제자리로 돌아간다?
+
+        // 걷기 코루틴 정지 & 되돌아가기 코루틴 시작
+        StopCoroutine(WalkCoroutine());
+        StartCoroutine(returnCoroutine());
+
+        // 일정 범위 내에 플레이어가 들어왔을 경우
+        if (Vector3.Distance(transform.position, player.transform.position) < rage)
+        {
+            animator.SetBool("isIdle", false);  // 애니메이션 취소
+            curState = State.Running;   // 추적상태로 변환
+        }
     }
 
+    // 대기모션과 배회모션의 코루틴
+    IEnumerator returnCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        curState = State.Return;
+        animator.SetBool("isWalking", false);
+    }
+
+    // 피격시 출력할 함수
     public void IsHit()
     {
-        // 피격
-        // 몬스터의 체력 감소
-        // 피격 에니메이션 재성
-        // 스턴
+        // 피격 애니메이션 출력
+        animator.SetBool("isHit", true);
+        animator.SetBool("isHit", false);
+
+        // Hp 감소
+        hp -= damage;
     }
 
-    // IEnumerator 
+    // 충돌 감지
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 충돌한 물체가 발사체일 경우(플레이어의 공격일 경우)
+        if (collision.gameObject.tag == "attack")   // 임의로 정해놓은 상태
+        {
+            curState = State.IsHit;
+        }
+    }
 
-    // (TODO)스턴상태일 경우 불러올 함수 작성
 }
+
